073cb89a25f6475e9ee8e45a351a76625e84c314
Kingsley Loo-Xu <kingsleyloolay@outlook.com>
2026-03-27 19:02:40 -0700

    updates

--- fan_pwm/fan_pwm.ino
+++ fan_pwm/fan_pwm.ino
@@ -1,14 +1,18 @@
-#include <LiquidCrystal.h>
-#include <MQ135.h>
+//IntelliAir code 
 
+
+#include <LiquidCrystal.h>
+
+//variables and constants 
 LiquidCrystal lcd(7, 8, 24, 26, 28, 30);
 
+const int mq135 = 0;
 const int tach = 2;
 const int fanPin = 3; 
 
 int speed = 0; 
 int newSpeed = 1; 
-
+int sensorValue; 
 void setup() {
   lcd.begin(16, 2);
   pinMode(fanPin, OUTPUT);
@@ -20,25 +24,47 @@ void setup() {
 
 void loop() {
   lcd.setCursor(0, 0);
+
+
   
   newSpeed = Serial.parseInt();
+
+  // reads input of the MQ135 sensor and displays it to the lcd
+  int sensorValue = analogRead(mq135); 
+  Serial.println(sensorValue);
+  lcd.print("air quality: ");
+  if (sensorValue > 99) {
+    lcd.print(sensorValue);
+
+  } else {
+    lcd.print(sensorValue);
+    lcd.print(" ");
+
+  }
+  lcd.print(sensorValue);
+
+
+  // handles the input of the fan speed 
+
   if (Serial.available() > 0) {
   
+    
 
     Serial.println("Fan speed is now: ");
 
-    lcd.clear();
+    lcd.setCursor(1, 0);
     lcd.print(newSpeed);
     speed = newSpeed;
     Serial.println(speed);
 
   }
   
-
+  //changes the fan speed, if there is a new input detected 
   analogWrite(fanPin, speed); 
 
   while(Serial.available() > 0) {
     Serial.read();  
   }
+
 
 }
